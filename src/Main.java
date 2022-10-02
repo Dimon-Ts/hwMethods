eabfaa70be3a9e1a0b4be59f598721d1a7bf8757
Dimon-Ts <tselishev.dimon@yandex.ru>
2022-10-03 02:43:57 +0300

    Массивы.Домашнее задание.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
-    public static void main(String[] args) {
 
+
+    public static void printYear(int year){
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("Год " + year + " является високосным");
+        } else {
+            System.out.println("Год " + year + " не является високосным");
+        }
+    }
+    public static void main(String[] args) {
+        // Методы.Домашнее задание.
+        System.out.println("Методы.Домашнее задание.");
+        System.out.println("Программа-определитель високосного года");
+        int year = 2031;
+        printYear(year);
     }
 }
